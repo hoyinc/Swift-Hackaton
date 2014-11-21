0387ab2c6686299adee758e12008949d50ed5e05
Sunny Cheung <sunnycyk@yahoo.com>
2014-11-21 21:44:46 +0800

    change View Controller

--- Numero/GameViewController.swift
+++ Numero/GameViewController.swift
@@ -11,57 +11,15 @@ import AddressBook
 
 class GameViewController: UIViewController {
     
-    var addressBook:ABAddressBookRef!
-    var people:NSArray!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        let status = ABAddressBookGetAuthorizationStatus()
-        if status == .Denied || status == .Restricted {
-            // user previously denied, to tell them to fix that in settings
-            return
-        }
         
-        // open it
-        
-        var error: Unmanaged<CFError>?
-        self.addressBook = ABAddressBookCreateWithOptions(nil, &error)?.takeRetainedValue()
-        if self.addressBook == nil {
-            println(error?.takeRetainedValue())
-            return
-        }
-        
-        // request permission to use it
-        
-           }
-    
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        ABAddressBookRequestAccessWithCompletion(self.addressBook) {
-            
-            (granted:Bool, error:CFError!) in
-            
-            if !granted {
-                // warn the user that because they just denied permission, this functionality won't work
-                // also let them know that they have to fix this in settings
-                return
-            }
-            
-            if let people = ABAddressBookCopyArrayOfAllPeople(self.addressBook)?.takeRetainedValue() as? NSArray {
-               
-            }
-            
-            // NSLog("%@", self.people);
-            
-        }
-
-        
-        
+                
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- Numero/ViewController.swift
+++ Numero/ViewController.swift
@@ -7,19 +7,57 @@
 //
 
 import UIKit
+import AddressBook
 
 class ViewController: UIViewController {
+
+    var addressBook:ABAddressBookRef!
+    var people:NSArray!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let status = ABAddressBookGetAuthorizationStatus()
+        if status == .Denied || status == .Restricted {
+            // user previously denied, to tell them to fix that in settings
+            return
+        }
+        
+        // open it
+        
+        var error: Unmanaged<CFError>?
+        self.addressBook = ABAddressBookCreateWithOptions(nil, &error)?.takeRetainedValue()
+        if self.addressBook == nil {
+            println(error?.takeRetainedValue())
+            return
+        }
+        
+        
+        requestAddressBookAccess()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func requestAddressBookAccess() {
+        ABAddressBookRequestAccessWithCompletion(addressBook) {
+            granted, error in
+            
+            if !granted {
+                // warn the user that because they just denied permission, this functionality won't work
+                // also let them know that they have to fix this in settings
+                return
+            }
+            
+           // if let people = ABAddressBookCopyArrayOfAllPeople(addressBook)?.takeRetainedValue() as? NSArray {
+                // now do something with the array of people
+            //}
+        }
+    }
+    
 }
 
